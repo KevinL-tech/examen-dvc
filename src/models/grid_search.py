b5cb5ca6011f6ebd01ce60c8f1a4d31300c92ef4
KevinL-tech <k.lakhdar@outlook.fr>
2025-06-05 18:33:09 +0200

    maj grid_search.py + add params.yaml

--- src/models/grid_search.py
+++ src/models/grid_search.py
@@ -2,15 +2,20 @@ import pandas as pd
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import GridSearchCV
 import pickle
+import yaml
 
 X_train = pd.read_csv("data/processed_data/X_train_scaled.csv")
 y_train = pd.read_csv("data/processed_data/y_train.csv").iloc[:, 0]
 
 model = RandomForestRegressor(random_state=42)
+
+with open("params.yaml", "r") as f :
+    params = yaml.safe_load(f)["grid_search"]
+
 param_grid = {
-    "n_estimators": [100, 200, 500],
-    "max_depth" : [2, 5, 10],
-    "min_samples_split" : [2, 3, 5]}
+    "n_estimators": params["n_estimators"],
+    "max_depth" : params["max_depth"],
+    "min_samples_split" : params["min_samples_split"]}
 
 grid_search = GridSearchCV(model, param_grid, cv=5, scoring="neg_root_mean_squared_error")
 grid_search.fit(X_train, y_train)
