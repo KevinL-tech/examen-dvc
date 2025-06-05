c7c1d529810e0be010c3532dfb2114a1dd4ccdb9
KevinL-tech <k.lakhdar@outlook.fr>
2025-06-05 12:57:14 +0200

    Correction error normalize_data.py

--- src/data/normalize_data.py
+++ src/data/normalize_data.py
@@ -5,6 +5,9 @@ X_train = pd.read_csv("data/processed_data/X_train.csv")
 X_test = pd.read_csv("data/processed_data/X_test.csv")
 
 scaler = StandardScaler()
+X_train_scaled = scaler.fit_transform(X_train)
+X_test_scaled = scaler.transform(X_test)
+
 X_train_scaled = pd.DataFrame(X_train_scaled, columns = X_train.columns)
 X_test_scaled = pd.DataFrame(X_test_scaled, columns = X_test.columns)
 
